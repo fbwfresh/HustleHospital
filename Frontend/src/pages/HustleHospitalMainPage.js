f060c93df06086420f11ecbacc303c1ce4dd843a
fbwfresh <joshua.dixon@mykenzie.snhu.edu>
2022-12-19 14:57:09 -0800

    working on the javascript for the homepage

---
+++ Frontend/src/pages/HustleHospitalMainPage.js
@@ -0,0 +1,89 @@
+import BaseClass from "../util/baseClass";
+import DataStore from "../util/DataStore";
+import ExampleClient from "../api/exampleClient";
+
+/**
+ * Logic needed for the view playlist page of the website.
+ */
+class HustleHospitalMainPage extends BaseClass {
+
+    constructor() {
+        super();
+        this.bindClassMethods(['onGet', 'onCreate', 'renderExample'], this);
+        this.dataStore = new DataStore();
+    }
+
+       /**
+         * Once the page has loaded, set up the event handlers and fetch the concert list.
+         */
+        async mount() {
+            document.getElementById('create-patientForm').addEventListener('submit', this.onGet);
+            document.getElementById('create-doctorForm').addEventListener('submit', this.onCreate);
+            //Change this new ExampleClient() to HospitalClient() when its made
+            this.client = new ExampleClient();
+
+            this.dataStore.addChangeListener(this.renderExample)
+        }
+
+ // Render Methods --------------------------------------------------------------------------------------------------
+
+ async renderExample() {
+        let resultArea = document.getElementById("result-info");
+
+    //todo: Not sure what dataStore.get(example) means
+        const example = this.dataStore.get("example");
+        if (example) {
+            resultArea.innerHTML = `
+                <div>ID: ${example.id}</div>
+                <div>Name: ${example.name}</div>
+            `
+        } else {
+            resultArea.innerHTML = "No Item";
+        }
+    }
+
+    // Event Handlers --------------------------------------------------------------------------------------------------
+/*ToDo I still have to do the Event Handlers and possibly name them something else */
+async onGet(event) {
+        // Prevent the page from refreshing on form submit
+        event.preventDefault();
+
+        let id = document.getElementById("id-field").value;
+        this.dataStore.set("example", null);
+
+        let result = await this.client.getExample(id, this.errorHandler);
+        this.dataStore.set("example", result);
+        if (result) {
+            this.showMessage(`Got ${result.name}!`)
+        } else {
+            this.errorHandler("Error doing GET!  Try again...");
+        }
+    }
+
+    async onCreate(event) {
+        // Prevent the page from refreshing on form submit
+        event.preventDefault();
+        this.dataStore.set("example", null);
+
+        let name = document.getElementById("create-name-field").value;
+
+        const createdExample = await this.client.createExample(name, this.errorHandler);
+        this.dataStore.set("example", createdExample);
+
+        if (createdExample) {
+            this.showMessage(`Created ${createdExample.name}!`)
+        } else {
+            this.errorHandler("Error creating!  Try again...");
+        }
+    }
+}
+
+/**
+ * Main method to run when the page contents have loaded.
+ */
+const main = async () => {
+    const HustleHospitalMainPage = new HustleHospitalMainPage();
+    HustleHospitalMainPage.mount();
+};
+
+window.addEventListener('DOMContentLoaded', main);
