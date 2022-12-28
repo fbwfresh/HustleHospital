28941f9ac5d50230df3af5f5f7ddb6e36c98f0ea
DigableBytes <104535170+Sboler@users.noreply.github.com>
2022-12-27 21:00:56 -0700

    Appointment Controller classes created

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     usedExports: true
   },
   entry: {
-    examplePage: path.resolve(__dirname, 'src', 'pages', 'examplePage.js'),
+    HustleHospitalPage: path.resolve(__dirname, 'src', 'pages', 'HustleHospitalPage.js'),
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -18,7 +18,7 @@ module.exports = {
     https: false,
     port: 8080,
     open: true,
-    openPage: 'http://localhost:8080',
+    openPage: 'http://localhost:8080/HustleHospital.html',
     // diableHostChecks, otherwise we get an error about headers and the page won't render
     disableHostCheck: true,
     contentBase: 'packaging_additional_published_artifacts',
@@ -27,8 +27,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
+      template: './src/HustleHospital.html',
+      filename: 'HustleHospital.html',
       inject: false
     }),
     new CopyPlugin({
